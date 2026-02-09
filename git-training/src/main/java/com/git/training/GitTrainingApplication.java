6132786f70b0484f07c509cda91119b987a8e2ca
Bernie Bautista <bernie@>
2026-02-09 18:31:41 +0800

    Initial commit

---
+++ git-training/src/main/java/com/git/training/GitTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.git.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrainingApplication.class, args);
+	}
+
+}
---
+++ git-training/src/test/java/com/git/training/GitTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
